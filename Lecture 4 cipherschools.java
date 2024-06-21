fdb7209c3afef26b85dcf29eb8600ab614c280d2
sreys04 <119521720+sreys04@users.noreply.github.com>
2024-06-21 13:06:13 +0530

    Lecture 4 cipherschools.java

---
+++ Lecture 4 cipherschools.java
@@ -0,0 +1,45 @@
+Features of JAVA:
+..............................................
+The Primary objectives of Java Programming language creation was to make it portable, simple and secure programming language . Apart from this, there are also some excellent features which play an important role in the popularity of this language.
+The features of Java are also known as Java buzzwords.
+
+A list of the most important features of the Java language is given:
+
+1. Simple.........
+
+   ->Java is very easy to learn, and its syntax is simple, clean and easy to understand
+
+2. Object Oriented........
+  
+   ->Java is an object-oriented programming language. Everything in Java is an object. Object-oriented means we organize our software as a combination of different types of objects that incorporate both data and behavior.
+
+3. Platform Dependent............
+   
+     ->Java is platform independent because it is different from other languages like C, C++, etc. which are compiled into platform specific machines while Java is a write once, run anywhere language. 
+
+4. Architecture Neutral........
+
+     ->Java is architecture neutral because there are no implementation dependent features, for example, the size of primitive types is fixed.
+
+5. Portable........
+
+      ->Java is portable because it facilitates you to carry the Java bytecode to any platform. It doesn't require any implementation.
+
+6. Robust........
+
+     ->The English mining of Robust is strong.
+7. Secure..........
+
+      ->Java is best known for its security. With Java, we can develop virus-free systems.
+
+8. High Performance..............
+
+      ->Java is faster than other traditional interpreted programming languages because Java bytecode is "close" to native code. It is still a little bit slower than a compiled language (e.g., C++). Java is an interpreted language that is why it is slower than compiled languages, e.g., C, C++, etc.
+
+9. Multi Threaded............
+
+      ->A thread is like a separate program, executing concurrently. We can write Java programs that deal with many tasks at once by defining multiple threads. The main advantage of multi-threading is that it doesn't occupy memory for each thread. It shares a common memory area. Threads are important for multi-media, Web applications, etc.
+
+10. Distributed............
+
+     ->Java is distributed because it facilitates users to create distributed applications in Java. RMI and EJB are used for creating distributed applications. This feature of Java makes us able to access files by calling the methods from any machine on the internet.
