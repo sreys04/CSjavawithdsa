cdf76b9191b929e7b51d9ff2b347968ef3d7ec61
sreys04 <119521720+sreys04@users.noreply.github.com>
2024-06-21 14:56:27 +0530

    Lecture 1 cipherschools.java

---
+++ Lecture 1 cipherschools.java
@@ -0,0 +1,24 @@
+Objects & Classes:
+........................................................
+
+Objects:-
+     
+ An entity that has state and behavior is known as an object.
+
+An object has three characteristics:
+    1. State: represents the data (value) of an object.
+    2. Behavior: represents the behavior (functionality) of an        object such as deposit, withdraw, etc.
+    3. Identity: An object identity is typically implemented via a unique ID. The value of the ID is not visible to the external user. 
+
+
+Classes:-
+
+ A class is a group of objects which have common properties. It is a template or blueprint from which objects are created. It is a logical entity. It can't be physical.
+
+A class in Java can contain:
+
+1. Fields
+2. Methods
+3. Constructors
+4. Blocks
+5. Nested class and interface
