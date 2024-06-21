9b93dc56baea07759a515083d9103543e5a0f31b
sreys04 <119521720+sreys04@users.noreply.github.com>
2024-06-21 14:45:40 +0530

    Lecture 6 cipherschools.java

---
+++ Lecture 6 cipherschools.java
@@ -0,0 +1,11 @@
+Instantiation
+...............................................
+In Java, instantiation mean to call the constructor of a class that creates an instance or object of the type of that class. In other words, creating an object of the class is called instantiation. It occupies the initial memory for the object and returns a reference. An object instantiation in Java provides the blueprint for the class.
+
+Syntax for Instantiation:-
+ClassName cn;  
+cn= new ClassName;  
+
+There are two ways to create instances:-
+1. using new keyword
+2. using static factory method
