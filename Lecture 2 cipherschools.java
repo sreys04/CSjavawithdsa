45184deda6ba90d5b54efc10de4c90e95cfdf8de
sreys04 <119521720+sreys04@users.noreply.github.com>
2024-06-21 15:04:56 +0530

    Lecture 2 cipherschools.java

---
+++ Lecture 2 cipherschools.java
@@ -0,0 +1,43 @@
+Git and GitHub:
+...........................................
+
+Git:-
+
+Git is an open-source distributed version control system. It is designed to handle minor to major projects with high speed and efficiency. It is developed to co-ordinate the work among the developers. The version control allows us to track and work together with our team members at the same workspace.
+
+Some remarkable features of Git are:
+1. Open Source
+2. Scalability
+3. Security
+4. Speed
+
+Benefits of Git:
+
+1.Save Time
+2. Offline Working
+3. Undo Mistakes
+4. Track Changes
+
+
+
+GitHub:-
+
+GitHub is an immense platform for code hosting. It supports version controlling and collaboration.It offers both distributed version control and source code management (SCM) functionality of Git. It also facilitates collaboration features such as bug tracking, feature requests, task management for every project.
+
+Essential components of the GitHub are:
+
+Repositories
+Branches
+Commits
+Pull Requests
+Git 
+
+Features of GitHub are;
+- collaboration
+- bug tracking
+- branches
+- git repositories
+- project management
+- team management
+-code hosting
+- track and assign tasks
