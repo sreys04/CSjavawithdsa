113d685a5d1d8d003b7d7c966e65287c438f341e
sreys04 <119521720+sreys04@users.noreply.github.com>
2024-06-21 13:25:26 +0530

    Lecture 5 cipherschools.java

---
+++ Lecture 5 cipherschools.java
@@ -0,0 +1,43 @@
+public class Operators{
+    public static void main(String[] args){
+        int x = 5;
+        int y = 7;
+        System.out.println("X is : " + x);
+         System.out.println("Y is : " + y);
+         
+         byte b= 10;
+         System.out.println("B is : " + b);
+         
+         int i = b;//Implicit type casting or widening or up-casting
+         System.out.println("I is : " + i);
+         
+         byte b2 =(byte)129;//Explicit type  casting or narrowing  or down-casting
+         System.out.println("B2 is : " + b2);
+         
+         byte b3 =(byte)128;
+         System.out.println("B3 is : " + b3);
+         
+         byte b4 =(byte)256;
+         System.out.println("B4 is : " + b4);
+         
+         float f=(float)5.5;//by default considers decimal to double
+         float f2=5.5f;
+         double d= 10.5;
+         char ch='a';
+         System.out.println(ch);
+         
+         int z = x+y;
+        
+        //Increment & Decrement Operators
+        int m =5;
+        int n =m++;
+        System.out.println("M is: "+m+",N is: "+n);
+        
+        int p =5;
+        int q =m++;
+        System.out.println("P is: "+p+",Q is: "+q);
+        
+         
+         
+    }
+}
