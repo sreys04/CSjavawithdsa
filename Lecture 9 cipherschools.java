2c070d3d0af92fad1adc330240c886991d26cb33
sreys04 <119521720+sreys04@users.noreply.github.com>
2024-06-23 16:20:55 +0530

    Lecture 9 cipherschools.java

---
+++ Lecture 9 cipherschools.java
@@ -0,0 +1,20 @@
+//implementation of objects and inheritance
+
+
+class Shape{   //parent or base or super class
+    double pi = 3.14;
+    double getSquare(int r){
+        return r*r;
+    }
+}
+class Circle extends Shape{   //child class,derived or sub class
+    void area(int r){
+        System.out.println(pi*getSquare(r));
+    }
+}
+public class InheritanceStudy{
+    public static void main(String[] args){
+        Circle c=new Circle();
+        c.area(5);
+    }
+}
