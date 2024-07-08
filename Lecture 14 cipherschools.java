2c7eace2319391cc0b3aba25e25cdce6f2ba2081
sreys04 <119521720+sreys04@users.noreply.github.com>
2024-07-08 19:34:11 +0530

    Lecture 14 cipherschools.java

---
+++ Lecture 14 cipherschools.java
@@ -0,0 +1,7 @@
+Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+In Java, array is an object of a dynamically generated class.
+
+ Java array inherits the Object class, and implements the Serializable as well as Cloneable interfaces. We can store primitive values or objects in an array in Java.
+
+ Like C/C++, we can also create single dimentional or multidimentional arrays in Java.
